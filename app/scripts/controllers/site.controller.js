d9416e8212085d6644d7ac539dca65d5f024f274
Peter McNeely <pete.mcneely@gmail.com>
2017-10-24 22:13:15 -0400

    Add searchcontent factory for handling google api.

--- app/scripts/controllers/site.controller.js
+++ app/scripts/controllers/site.controller.js
@@ -5,9 +5,16 @@
         .module('site')
         .controller('siteCtrl', siteCtrl);
 
-    siteCtrl.$inject = ['$rootScope', 'MetaService'];
+    siteCtrl.$inject = ['$rootScope', 'MetaService', 'SearchContentFactory'];
 
-    function siteCtrl($rootScope, MetaService) {
+    function siteCtrl($rootScope, MetaService, SearchContentFactory) {
         $rootScope.metaService = MetaService;
+        $rootScope.searchContentFactory = SearchContentFactory;
+
+        var client_id = '672771660775-p4sc645s0769u530ia62lepao7ju32js.apps.googleusercontent.com';
+
+        gapi.load("client:auth2", function () {
+            gapi.auth2.init({ client_id: client_id });
+        });
     }
 })();
---
+++ app/scripts/factories/searchContent.factory.js
@@ -0,0 +1,67 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('site')
+        .factory('SearchContentFactory', searchContent);
+
+    searchContent.$inject = ['$http', '$q'];
+
+    function searchContent($http, $q) {
+        var service = {
+            sitemaps: execute
+        };
+
+        var authenticated = false;
+
+        return service;
+
+        function authenticate() {
+            return gapi.auth2.getAuthInstance()
+                .signIn({ scope: "https://www.googleapis.com/auth/webmasters https://www.googleapis.com/auth/webmasters.readonly" })
+                .then(function () {
+                    console.log("Sign-in successful");
+                }, function (error) {
+                    console.error("Error signing in", error);
+                });
+        }
+        function loadClient() {
+            return gapi.client.load("https://content.googleapis.com/discovery/v1/apis/webmasters/v3/rest")
+                .then(function () {
+                    console.log("GAPI client loaded for API");
+                }, function (error) {
+                    console.error("Error loading GAPI client for API");
+                });
+        }
+
+        // Make sure the client is loaded and sign-in is complete before calling this method.
+        function execute() {
+            authenticateAndLoad().then(
+                function () {
+                    return gapi.client.webmasters.sitemaps.list({siteUrl: 'https://petermcneely.com'})
+                        .then(function (response) {
+                            // Handle the results here (response.result has the parsed body).
+                            console.log("Response", response);
+                        }, function (error) {
+                            console.error("Execute error", error);
+                        });
+                }
+            );
+        }
+
+        function authenticateAndLoad() {
+            if (!authenticated) {
+                return authenticate().then(
+                    loadClient().then(
+                        function () {
+                            authenticated = true;
+                        }
+                    )
+                );
+            }
+            else {
+                return $q.resolve();
+            }
+        }
+    }
+})();
